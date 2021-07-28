b43b32cc22f78feacddfa34bbc5499853c6aecb5
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-27 21:44:09 -0400

    commit tree f5a43a7472e84b4073aee70da94cf8a64dc6557b

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // Code your solutions in this file
-// const names = ["Lisa", "Kaitlin", "Jan"]
-// const event = ["surprise"]
-
-// function writeCards(names, event){
-//   for (let i = 0; i < cards.length; i++) {
-//     console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!)
-//   }
-//   return names, event;
-// }
-
 const cards = [[ 'Lisa', 'Kaitlin', 'Jan' ], ['surprise']];
  
 function writeCards(cards) {
